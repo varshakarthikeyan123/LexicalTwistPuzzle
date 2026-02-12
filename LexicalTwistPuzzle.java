0230722bcffbf0f4c2c30c16e70f82a72248c788
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-02-12 15:59:14 +0530

    [Varsha] : Added : implement UC5 - transform word on reverse match

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -29,9 +29,56 @@ public class LexicalTwistPuzzle {
         String reversed = new StringBuilder(firstWord).reverse().toString();
 
         if (reversed.equalsIgnoreCase(secondWord)) {
-            System.out.println("The second word is the reverse of the first word.");
-        } else {
-            System.out.println("The words are not reverse of each other.");
+
+            // UC5 Transformation
+            String transformed = reversed.toLowerCase()
+                    .replaceAll("[aeiou]", "@");
+
+            System.out.println(transformed);
+
+        }
+
+        scanner.close();
+    }
+}
+import java.util.Scanner;
+
+public class LexicalTwistPuzzle {
+
+    public static void main(String[] args) {
+
+        System.out.println("Lexical Twist Puzzle");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String firstWord = scanner.nextLine();
+
+        System.out.print("Enter second word: ");
+        String secondWord = scanner.nextLine();
+
+        // UC3 Validation
+        if (firstWord.contains(" ")) {
+            System.out.println(firstWord + " is an invalid word");
+            return;
+        }
+
+        if (secondWord.contains(" ")) {
+            System.out.println(secondWord + " is an invalid word");
+            return;
+        }
+
+        // UC4 Reverse Check
+        String reversed = new StringBuilder(firstWord).reverse().toString();
+
+        if (reversed.equalsIgnoreCase(secondWord)) {
+
+            // UC5 Transformation
+            String transformed = reversed.toLowerCase()
+                    .replaceAll("[aeiou]", "@");
+
+            System.out.println(transformed);
+
         }
 
         scanner.close();
