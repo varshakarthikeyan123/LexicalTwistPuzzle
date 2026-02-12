102729aa1718c100b9381321160790a780ff711e
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-02-12 15:50:25 +0530

    [Varsha] : Added : implement UC2 - accept two word inputs

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
 
     public static void main(String[] args) {
+
         System.out.println("Lexical Twist Puzzle");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String firstWord = scanner.nextLine();
+
+        System.out.print("Enter second word: ");
+        String secondWord = scanner.nextLine();
+
+        scanner.close();
     }
 }
